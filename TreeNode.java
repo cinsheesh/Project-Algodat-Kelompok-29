4e03b12e4305c0ef33518b4cc2774e7d177f1027
srzall <rizalsamsul2136@gmail.com>
2024-12-18 20:05:31 +0600

    tree

--- TreeNode.java
+++ TreeNode.java
@@ -34,5 +34,26 @@ class Tree {
             root.right = new TreeNode(division);
         }
     }
+    public void addStaffToDivision(TreeNode node, String name, int id, String position) {
+        if (node != null) {
+            node.staffList.insertLast(new Node(name, id, position));
+        }
+    }
 
+    public void traverse() {
+        traverse(root, 0);
+    }
+
+    private void traverse(TreeNode node, int level) {
+        if (node == null) {
+            return;
+        }
+        System.out.println("  ".repeat(level) + "- " + node.division);
+        System.out.println("  ".repeat(level) + "  Staff:");
+        node.staffList.display();
+        traverse(node.left, level + 1);
+        traverse(node.middle, level + 1);
+        traverse(node.right, level + 1);
+    }
+}
    
